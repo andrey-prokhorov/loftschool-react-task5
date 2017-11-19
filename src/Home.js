284e4c45fc5e71a5f8700799ea18022c1ce5c40f
Andrey Prokhorov <andrey@ezy.se>
2017-11-19 19:07:49 +0100

    Need redux to get the login event in home component

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,8 @@
-import React, { Component } from "react";
-import { addListener, removeListener } from "./AuthorizeApi";
+import React from "react";
 
-class Home extends Component {
-  state = {
-    isAuthorized: false
-  };
-
-  componentDidMount() {
-    addListener(this.handleAuthorize);
-  }
-
-  componentWillUnmount() {
-    removeListener(this.handleAuthorize);
-  }
-
-  handleAuthorize = isAuthorized => {
-    this.setState({ isAuthorized });
-  };
-
-  render() {
-    const { isAuthorized } = this.state;
-
-    return (
-      <div className="container">
-        <h3>Home page</h3>
-        <p>Welcome to the main page!</p>
-
-        <p
-          style={{ visibility: isAuthorized ? "visible" : "hidden" }}
-        >
-          You are logged in!
-        </p>
-      </div>
-    );
-  }
-}
-
-export default Home;
+export default () => (
+  <div className="container">
+    <h3>Home page</h3>
+    <p>Welcome to the main page!</p>
+  </div>
+);
